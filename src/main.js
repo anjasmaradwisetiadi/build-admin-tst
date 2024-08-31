9c7dc81781d7a025bdf34ea8bae4b9437a6344e4
diamondto <enzoblackred@gmail.com>
2024-08-31 10:19:45 +0700

    [feat/primevue] add lara vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import './index.css'
+import '@/assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Lara from '@/presets/lara';  
 
 import 'material-symbols';
 
@@ -11,6 +14,10 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia());
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Lara    
+});
 app.use(router)
 
 app.mount('#app')
--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -5,8 +5,26 @@
                 <div class="bg-blue-600 text-white py-2 px-4 rounded-t-md">
                     <h1 class="text-lg font-semibold">Invitations Labels Table</h1>
                 </div>
-
                 <div class="p-4 border rounded-b-md bg-white">
+                    <div class="card flex justify-center">
+                        <Select v-model="selectedCountry" :options="countries" filter optionLabel="name" placeholder="Select a Country" class="w-full md:w-56">
+                            <template #value="slotProps">
+                                <div v-if="slotProps.value" class="flex items-center">
+                                    <img :alt="slotProps.value.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
+                                    <div>{{ slotProps.value.name }}</div>
+                                </div>
+                                <span v-else>
+                                    {{ slotProps.placeholder }}
+                                </span>
+                            </template>
+                            <template #option="slotProps">
+                                <div class="flex items-center">
+                                    <img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+                                    <div>{{ slotProps.option.name }}</div>
+                                </div>
+                            </template>
+                        </Select>
+                    </div>
                     <div class="flex justify-end my-4 space-x-2">
                         <button class="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800">RESET</button>
                         <button class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">APPLY</button>
@@ -58,6 +76,21 @@
 <script setup>
 import { ref, watch, onMounted, onUpdated, onBeforeMount, computed} from 'vue';
 import { dataDummyEmployee } from '@/utilize/DataDummy';
+import Select from 'primevue/select';
+
+const selectedCountry = ref();
+const countries = ref([
+    { name: 'Australia', code: 'AU' },
+    { name: 'Brazil', code: 'BR' },
+    { name: 'China', code: 'CN' },
+    { name: 'Egypt', code: 'EG' },
+    { name: 'France', code: 'FR' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'India', code: 'IN' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'Spain', code: 'ES' },
+    { name: 'United States', code: 'US' }
+]);
 
 const datas = ref(dataDummyEmployee)
 
