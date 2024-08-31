7172622b2385e581a5c28fc389a41af0f591d41a
diamondto <enzoblackred@gmail.com>
2024-08-31 07:16:33 +0700

    [feat/auth] add dummy data for order and list

--- src/utilize/DataDummy.js
+++ src/utilize/DataDummy.js
@@ -1,3 +1,399 @@
+export const listCoupoun = {
+  "response_code": "2000000",
+  "response_message": "OK",
+  "items": [
+      {
+          "code": "aSASj288",
+          "name": "apasi",
+          "start_date": "2024-08-30",
+          "end_date": "2024-11-12",
+          "created_at": "2024-08-30 12:27:15"
+      },
+      {
+          "code": "Awj27628",
+          "name": "apaya",
+          "start_date": "2024-08-22",
+          "end_date": "2024-10-23",
+          "created_at": "2024-08-29 23:13:43"
+      },
+      {
+          "code": "ASDjiks1",
+          "name": "code test",
+          "start_date": "2024-08-23",
+          "end_date": "2024-09-23",
+          "created_at": "2024-08-29 09:51:37"
+      },
+      {
+          "code": "FVuzCCoX",
+          "name": "COUPON-01799",
+          "start_date": "2023-03-01",
+          "end_date": "2023-07-23",
+          "created_at": "2022-02-07 22:25:29"
+      },
+      {
+          "code": "SaXzLRus",
+          "name": "COUPON-02198",
+          "start_date": "2024-07-10",
+          "end_date": "2024-07-24",
+          "created_at": "2024-04-11 08:06:40"
+      },
+      {
+          "code": "JFDKKQ9E",
+          "name": "COUPON-06301",
+          "start_date": "2024-07-05",
+          "end_date": "2024-09-16",
+          "created_at": "2022-09-14 03:44:49"
+      },
+      {
+          "code": "Vk1QZomI",
+          "name": "COUPON-07235",
+          "start_date": "2024-08-22",
+          "end_date": "2024-10-02",
+          "created_at": "2024-08-19 19:36:57"
+      },
+      {
+          "code": "IYLXNnQz",
+          "name": "COUPON-10325",
+          "start_date": "2022-05-17",
+          "end_date": "2023-07-27",
+          "created_at": "2022-04-27 02:29:02"
+      },
+      {
+          "code": "zVGv4ng0",
+          "name": "COUPON-14277",
+          "start_date": "2023-06-03",
+          "end_date": "2023-06-25",
+          "created_at": "2022-05-25 19:58:35"
+      },
+      {
+          "code": "wKEfGszr",
+          "name": "COUPON-14457",
+          "start_date": "2023-06-18",
+          "end_date": "2023-10-06",
+          "created_at": "2023-04-30 20:12:34"
+      }
+  ],
+  "current_page": 1,
+  "last_page": 5,
+  "per_page": 10,
+  "total": 48
+}
+
+export const detailCoupoun = {
+  "response_code": "2000000",
+  "response_message": "OK",
+  "code": "FVuzCCoX",
+  "name": "COUPON-01799",
+  "start_date": "2023-03-01",
+  "end_date": "2023-07-23",
+  "created_at": "2022-02-07 22:25:29"
+}
+
+export const listOrders = {
+  "response_code": "2000000",
+  "response_message": "OK",
+  "items": [
+      {
+          "invoice_no": "INV19982960338729385",
+          "grandtotal": "46541000.00",
+          "created_at": "2024-10-06 08:55:30",
+          "buyer": {
+              "name": "Ira Wulandari",
+              "phone": "089546446740"
+          },
+          "store": {
+              "code": "71979183",
+              "name": "Indah Purnawati"
+          },
+          "coupon": {
+              "code": "SaXzLRus",
+              "name": "COUPON-02198"
+          }
+      },
+      {
+          "invoice_no": "INV07281931730166667",
+          "grandtotal": "41583500.00",
+          "created_at": "2023-12-16 05:06:35",
+          "buyer": {
+              "name": "Yance Hutapea",
+              "phone": "086492270704"
+          },
+          "store": {
+              "code": "64600737",
+              "name": "Hartaka Haryanto"
+          },
+          "coupon": {
+              "code": null,
+              "name": null
+          }
+      },
+      {
+          "invoice_no": "INV57143495573175944",
+          "grandtotal": "40070000.00",
+          "created_at": "2024-07-18 12:34:31",
+          "buyer": {
+              "name": "Wakiman Tarihoran",
+              "phone": "081285663838"
+          },
+          "store": {
+              "code": "67668682",
+              "name": "Amalia Handayani"
+          },
+          "coupon": {
+              "code": null,
+              "name": null
+          }
+      },
+      {
+          "invoice_no": "INV84347024823275815",
+          "grandtotal": "37560600.00",
+          "created_at": "2024-01-20 03:43:35",
+          "buyer": {
+              "name": "Daryani Budiyanto",
+              "phone": "083197679917"
+          },
+          "store": {
+              "code": "09296892",
+              "name": "Vinsen Pradipta"
+          },
+          "coupon": {
+              "code": null,
+              "name": null
+          }
+      },
+      {
+          "invoice_no": "INV42773633012125532",
+          "grandtotal": "36676000.00",
+          "created_at": "2024-06-22 22:34:15",
+          "buyer": {
+              "name": "Sidiq Salahudin",
+              "phone": "084569041553"
+          },
+          "store": {
+              "code": "26748257",
+              "name": "Kanda Wasita"
+          },
+          "coupon": {
+              "code": null,
+              "name": null
+          }
+      },
+      {
+          "invoice_no": "INV27720538003553393",
+          "grandtotal": "35976700.00",
+          "created_at": "2023-10-31 20:24:53",
+          "buyer": {
+              "name": "Karsana Sitorus",
+              "phone": "087865113970"
+          },
+          "store": {
+              "code": "91221394",
+              "name": "Chandra Firgantoro"
+          },
+          "coupon": {
+              "code": "Yfihi549",
+              "name": "COUPON-73988"
+          }
+      },
+      {
+          "invoice_no": "INV30755839469151994",
+          "grandtotal": "35834800.00",
+          "created_at": "2024-04-05 12:48:23",
+          "buyer": {
+              "name": "Dian Pratama",
+              "phone": "082627450186"
+          },
+          "store": {
+              "code": "11552693",
+              "name": "Ulva Karimah Hassanah S.IP"
+          },
+          "coupon": {
+              "code": "2sojGNik",
+              "name": "COUPON-95996"
+          }
+      },
+      {
+          "invoice_no": "INV39020794095319765",
+          "grandtotal": "34866900.00",
+          "created_at": "2024-09-28 09:44:30",
+          "buyer": {
+              "name": "Kambali Pangestu",
+              "phone": "082705172146"
+          },
+          "store": {
+              "code": "04134233",
+              "name": "Azalea Uyainah"
+          },
+          "coupon": {
+              "code": "mJeue0jK",
+              "name": "COUPON-62996"
+          }
+      },
+      {
+          "invoice_no": "INV20019603865768690",
+          "grandtotal": "34695300.00",
+          "created_at": "2023-10-14 22:52:00",
+          "buyer": {
+              "name": "Vivi Sudiati",
+              "phone": "088936552012"
+          },
+          "store": {
+              "code": "53698935",
+              "name": "Lili Susanti"
+          },
+          "coupon": {
+              "code": null,
+              "name": null
+          }
+      },
+      {
+          "invoice_no": "INV54830685903256958",
+          "grandtotal": "34160000.00",
+          "created_at": "2024-10-26 01:19:48",
+          "buyer": {
+              "name": "Bahuwirya Maryati",
+              "phone": "083548497223"
+          },
+          "store": {
+              "code": "63157582",
+              "name": "Ira Novitasari M.TI."
+          },
+          "coupon": {
+              "code": null,
+              "name": null
+          }
+      }
+  ],
+  "current_page": 1,
+  "last_page": 215,
+  "per_page": 10,
+  "total": 2148
+}
+
+export const detailOrders = {
+  "response_code": "2000000",
+  "response_message": "OK",
+  "invoice_no": "INV19982960338729385",
+  "grandtotal": "46541000.00",
+  "created_at": "2024-10-06 08:55:30",
+  "buyer": {
+      "name": "Ira Wulandari",
+      "phone": "089546446740"
+  },
+  "store": {
+      "code": "71979183",
+      "name": "Indah Purnawati",
+      "city": "Kabupaten Aceh Singkil",
+      "province": "Aceh"
+  },
+  "coupon": {
+      "code": "SaXzLRus",
+      "name": "COUPON-02198"
+  },
+  "items": [
+      {
+          "product": {
+              "code": "13953102",
+              "name": "ACRYLIC EXT PUTIH ANGGREK",
+              "category": "Cat Acrylic Exterior",
+              "price": "1030000.00"
+          },
+          "qty": 1,
+          "total_price": "1030000.00"
+      },
+      {
+          "product": {
+              "code": "23744245",
+              "name": "ACRYLIC INT FIESTA ORANGE",
+              "category": "Cat Acrylic Interior",
+              "price": "96300.00"
+          },
+          "qty": 10,
+          "total_price": "963000.00"
+      },
+      {
+          "product": {
+              "code": "46453188",
+              "name": "ACRYLIC EXT SPRING YELLOW",
+              "category": "Cat Acrylic Exterior",
+              "price": "250000.00"
+          },
+          "qty": 7,
+          "total_price": "1750000.00"
+      },
+      {
+          "product": {
+              "code": "38866562",
+              "name": "ACRYLIC INT VIESTA BLUE",
+              "category": "Cat Acrylic Interior",
+              "price": "30000.00"
+          },
+          "qty": 1,
+          "total_price": "30000.00"
+      },
+      {
+          "product": {
+              "code": "97563119",
+              "name": "ACRYLIC INT OCEAN BLUE",
+              "category": "Cat Acrylic Interior",
+              "price": "480000.00"
+          },
+          "qty": 7,
+          "total_price": "3360000.00"
+      },
+      {
+          "product": {
+              "code": "53020552",
+              "name": "ACRYLIC INT STRAWBERRY",
+              "category": "Cat Acrylic Interior",
+              "price": "508800.00"
+          },
+          "qty": 10,
+          "total_price": "5088000.00"
+      },
+      {
+          "product": {
+              "code": "03061234",
+              "name": "ACRYLIC EXT APRICOT",
+              "category": "Cat Acrylic Exterior",
+              "price": "1100000.00"
+          },
+          "qty": 10,
+          "total_price": "11000000.00"
+      },
+      {
+          "product": {
+              "code": "49302329",
+              "name": "ACRYLIC EXT PUTIH KEBIRUAN",
+              "category": "Cat Acrylic Exterior",
+              "price": "1020000.00"
+          },
+          "qty": 6,
+          "total_price": "6120000.00"
+      },
+      {
+          "product": {
+              "code": "60538732",
+              "name": "ACRYLIC EXT CANDY RED",
+              "category": "Cat Acrylic Exterior",
+              "price": "1000000.00"
+          },
+          "qty": 7,
+          "total_price": "7000000.00"
+      },
+      {
+          "product": {
+              "code": "69389929",
+              "name": "ACRYLIC EXT SUNNY YELLOW",
+              "category": "Cat Acrylic Exterior",
+              "price": "1020000.00"
+          },
+          "qty": 10,
+          "total_price": "10200000.00"
+      }
+  ]
+}
+
 export const dataDummyEmployee = [
     {
       id:'1',
@@ -1199,4 +1595,4 @@ export const dataDummyEmployee = [
         group: "A",
         description: "Tina is a UI/UX designer."
       }
-  ];
+];
--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -1,13 +1,54 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">order Component</div>
+    <div class="w-full mx-6">
+        <div class="relative overflow-x-auto">
+            <div class="p-4">
+                <div class="bg-blue-600 text-white py-2 px-4 rounded-t-md">
+                    <h1 class="text-lg font-semibold">Invitations Labels Table</h1>
+                </div>
 
-                    <div class="card-body">
-                        I'm an order component.
+                <div class="p-4 border rounded-b-md bg-white">
+                    <div class="flex justify-end my-4 space-x-2">
+                        <button class="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800">RESET</button>
+                        <button class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">APPLY</button>
+                        <button class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">ADD</button>
                     </div>
+
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-black text-white">
+                            <tr>
+                                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Item</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">Komunitas</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-green-600 hover:underline mr-4">DETAIL</a>
+                                    <a href="#" class="text-yellow-600 hover:underline mr-4">EDIT</a>
+                                    <a href="#" class="text-red-600 hover:underline">DELETE</a>
+                                </td>
+                            </tr>
+                            <!-- Repeat rows as needed -->
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">Data 16</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-green-600 hover:underline mr-4">DETAIL</a>
+                                    <a href="#" class="text-yellow-600 hover:underline mr-4">EDIT</a>
+                                    <a href="#" class="text-red-600 hover:underline">DELETE</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">Data 15</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-green-600 hover:underline mr-4">DETAIL</a>
+                                    <a href="#" class="text-yellow-600 hover:underline mr-4">EDIT</a>
+                                    <a href="#" class="text-red-600 hover:underline">DELETE</a>
+                                </td>
+                            </tr>
+                            <!-- Additional rows as needed -->
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -15,6 +56,10 @@
 </template>
 
 <script setup>
+import { ref, watch, onMounted, onUpdated, onBeforeMount, computed} from 'vue';
+import { dataDummyEmployee } from '@/utilize/DataDummy';
+
+const datas = ref(dataDummyEmployee)
 
 </script>
 
